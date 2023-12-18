75cecc0ea72a7701a41b5f56120a1193397e7041
jefferson <berronesjefferson@gmail.com>
2023-12-18 10:56:05 -0500

    feat: custom form password

--- apps/security/views/profile.py
+++ apps/security/views/profile.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from apps.security.forms.profile import ProfileForm
+from apps.security.forms.user import MyPasswordChangeForm2
 from apps.security.mixins.mixins import PermissionMixin, UpdateViewMixin
 from apps.security.models import User
 from django.views.generic import UpdateView
@@ -21,33 +22,13 @@ class ProfileUpdateView(PermissionMixin,UpdateViewMixin, UpdateView):
         context['back_url'] = self.success_url
         return context
     
+
 class UserPasswordUpdateView(PermissionMixin,UpdateViewMixin,UpdateView):
     model = User
     template_name = 'profile/change_password.html'
-    form_class = PasswordChangeForm
-    success_url = reverse_lazy('security:auth_login')
+    form_class = MyPasswordChangeForm2
+    success_url = reverse_lazy('home')
     permission_required = 'change_userpassword'
-
-    def get_form(self, form_class=None):
-        form = self.form_class(user=self.request.user)
-
-        field_common = {
-            'class': 'form-control',
-            'autocomplete': 'off',
-        }
-        form.fields['old_password'].widget.attrs = {
-            **field_common,
-            'placeholder': 'Ingrese su contraseña actual',
-        }
-        form.fields['new_password1'].widget.attrs = {
-            **field_common,
-            'placeholder': 'Ingrese su nueva contraseña',
-        }
-        form.fields['new_password2'].widget.attrs = {
-            **field_common,
-            'placeholder': 'Repita su contraseña',
-        }
-        return form
 
     def get_object(self, queryset=None):
         return self.request.user
